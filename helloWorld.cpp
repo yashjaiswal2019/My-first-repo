48401745ad8f987760a54831b1e4027d154054da
yashjaiswal2019 <yashjaiswal2019@gmail.com>
2026-03-31 12:47:56 +0530

    Added hello world program.

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout<<"Hello world this is Yash, how are you?";
+    cout<<"Hello world this is Yash Jayasaval, how are you?";
     return 0;
 
 }
