2be99448186eadc0f6450684ce9a64af531745b3
yashjaiswal2019 <yashjaiswal2019@gmail.com>
2026-03-31 13:49:12 +0530

    completed num swap program

--- numSwap.cpp
+++ numSwap.cpp
@@ -1,1 +1,18 @@
-#include iostream 
+#include <iostream>
+using namespace std;
+int main(){
+
+int a,b,c;
+cout<<"enter the values of"<<endl<<"A: ";
+cin>>a;
+
+cout<<"B: ";
+cin>>b;
+
+c=a;
+a=b;
+b=c;
+
+cout<<"swapped values are"<<endl<<"A: "<<a<<endl<<"b: "<<b;
+return 0;
+}
