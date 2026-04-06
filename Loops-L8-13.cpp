f6757a3f48a2317e11f1fe9eec2554e255f2370d
yashjaiswal2019 <yashjaiswal2019@gmail.com>
2026-04-06 08:01:51 +0530

    solved Armstrong number question.

--- Loops-L8-13.cpp
+++ Loops-L8-13.cpp
@@ -6,8 +6,16 @@ int main()
     int n=500;
     int sum=0;
 
-    for(int i=1 ; i<=n ; i++){
-        
-    }
+    for(int i=100 ; i<=n ; i++){
+        int sum=0;
+        int t=i;
 
+        while (t!=0){
+            sum+=(t%10)*(t%10)*(t%10);
+            t/=10;
+        }
+        if (sum==i){
+            cout<<sum<<endl;
+        }
+    }
 }
